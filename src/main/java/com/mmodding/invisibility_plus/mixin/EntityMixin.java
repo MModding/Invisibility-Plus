7ae187d2c7311f98ed93d7480438dc6d7eddcac4
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-08-12 23:36:30 +0200

    Modify LivingEntityMixin and EntityMixin

--- src/main/java/com/mmodding/invisibility_plus/mixin/EntityMixin.java
+++ src/main/java/com/mmodding/invisibility_plus/mixin/EntityMixin.java
@@ -4,14 +4,26 @@ import com.mmodding.invisibility_plus.Utils;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Entity.class)
-public class EntityMixin {
+public abstract class EntityMixin {
+
+	@Shadow
+	public abstract void emitGameEvent(GameEvent event);
 
 	@Inject(method = "playStepSound", at = @At(value = "HEAD"), cancellable = true)
 	private void conditionalStepSound(BlockPos pos, BlockState state, CallbackInfo ci) {
@@ -24,6 +36,30 @@ public class EntityMixin {
 	private void conditionalSwimSound(float volume, CallbackInfo ci) {
 		if ((Entity) (Object) this instanceof LivingEntity livingEntity) {
 			Utils.checkInvisibilityAmplifierAndRun(livingEntity, 1, ci::cancel);
+		}
+	}
+
+	@ModifyArgs(method = "onSwimmingStart", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;playSound(Lnet/minecraft/sound/SoundEvent;FF)V"))
+	private void conditionalSplashSound(Args args) {
+		if ((Entity) (Object) this instanceof LivingEntity livingEntity) {
+			Utils.checkInvisibilityAmplifierAndRun(livingEntity, 1, () -> args.set(1, 0.0F));
+		}
+	}
+
+	@Inject(method = "onSwimmingStart", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;floor(D)I"), cancellable = true)
+	private void conditionalSwimParticles(CallbackInfo ci) {
+		if ((Entity) (Object) this instanceof LivingEntity livingEntity) {
+			Utils.checkInvisibilityAmplifierAndRun(livingEntity, 2, () -> {
+				this.emitGameEvent(GameEvent.SPLASH);
+				ci.cancel();
+			});
+		}
+	}
+
+	@Inject(method = "shouldSpawnSprintingParticles", at = @At(value = "HEAD"), cancellable = true)
+	private void conditionalSprintingParticles(CallbackInfoReturnable<Boolean> cir) {
+		if ((Entity) (Object) this instanceof LivingEntity livingEntity) {
+			Utils.checkInvisibilityAmplifierAndRun(livingEntity, 2, () -> cir.setReturnValue(false));
 		}
 	}
 }
--- src/main/java/com/mmodding/invisibility_plus/mixin/LivingEntityMixin.java
+++ src/main/java/com/mmodding/invisibility_plus/mixin/LivingEntityMixin.java
@@ -1,7 +1,10 @@
 package com.mmodding.invisibility_plus.mixin;
 
 import com.mmodding.invisibility_plus.Utils;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,12 +15,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class LivingEntityMixin extends EntityMixin {
 
 	@Inject(method = "tickStatusEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), cancellable = true)
-	private void conditionalParticles(CallbackInfo ci) {
-		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) ((Object) this), 2, ci::cancel);
+	private void conditionalEffectParticles(CallbackInfo ci) {
+		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) (Object) this, 2, ci::cancel);
+	}
+
+	@Inject(method = "fall", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnParticles(Lnet/minecraft/particle/ParticleEffect;DDDIDDDD)I"), cancellable = true)
+	private void conditionalFallParticles(double heightDifference, boolean onGround, BlockState landedState, BlockPos landedPosition, CallbackInfo ci) {
+		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) (Object) this, 2, ci::cancel);
+	}
+
+	@Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), cancellable = true)
+	private void conditionalDrownParticles(CallbackInfo ci) {
+		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) (Object) this, 2, ci::cancel);
+	}
+
+	@Inject(method = "displaySoulSpeedEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), cancellable = true)
+	private void conditionalSoulSpeedParticles(CallbackInfo ci) {
+		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) (Object) this, 2, ci::cancel);
 	}
 
 	@Inject(method = "getArmorVisibility", at = @At(value = "RETURN"), cancellable = true)
 	private void armorVisibility(CallbackInfoReturnable<Float> cir) {
-		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) ((Object) this), 3, () -> cir.setReturnValue(0.1F));
+		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) (Object) this, 3, () -> cir.setReturnValue(0.1F));
+	}
+
+	@Inject(method = "spawnItemParticles", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), cancellable = true)
+	private void conditionalItemParticles(ItemStack stack, int count, CallbackInfo ci) {
+		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) (Object) this, 4, ci::cancel);
 	}
 }
