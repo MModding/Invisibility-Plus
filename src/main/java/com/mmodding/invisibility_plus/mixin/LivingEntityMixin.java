00a6dd660231ca8477714f1b3ebbecdec7661255
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-08-12 19:31:19 +0200

    Add LivingEntityMixin#conditionalParticles

--- src/main/java/com/mmodding/invisibility_plus/mixin/LivingEntityMixin.java
+++ src/main/java/com/mmodding/invisibility_plus/mixin/LivingEntityMixin.java
@@ -5,10 +5,16 @@ import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends EntityMixin {
+
+	@Inject(method = "tickStatusEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), cancellable = true)
+	private void conditionalParticles(CallbackInfo ci) {
+		Utils.checkInvisibilityAmplifierAndRun((LivingEntity) ((Object) this), 2, ci::cancel);
+	}
 
 	@Inject(method = "getArmorVisibility", at = @At(value = "RETURN"), cancellable = true)
 	private void armorVisibility(CallbackInfoReturnable<Float> cir) {
