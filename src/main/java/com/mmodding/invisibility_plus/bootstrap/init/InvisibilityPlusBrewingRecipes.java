1975733cc7cd50390a95ed400dd50c7ac3d4f2bb
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-09-09 00:54:36 +0200

    Add InvisibilityPlusBrewingRecipes Change InvisibilityPlusBootstrap Update MModding Library

--- src/main/java/com/mmodding/invisibility_plus/bootstrap/init/InvisibilityPlusBrewingRecipes.java
+++ src/main/java/com/mmodding/invisibility_plus/bootstrap/init/InvisibilityPlusBrewingRecipes.java
@@ -1,15 +1,20 @@
-package com.mmodding.invisibility_plus;
+package com.mmodding.invisibility_plus.bootstrap.init;
 
 import com.mmodding.invisibility_plus.init.InvisibilityPotions;
-import com.mmodding.mmodding_lib.library.base.MModdingBootStrapInitializer;
+import com.mmodding.mmodding_lib.library.initializers.BootstrapElementsInitializer;
+import com.mmodding.mmodding_lib.library.initializers.invokepoints.BootstrapInvokePoint;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potions;
-import org.quiltmc.loader.api.ModContainer;
 
-public class InvisibilityPlusBootStrap implements MModdingBootStrapInitializer {
+public class InvisibilityPlusBrewingRecipes implements BootstrapElementsInitializer {
 
 	@Override
-	public void onInitializeBootStrap(ModContainer mod) {
+	public BootstrapInvokePoint getInvokePoint() {
+		return BootstrapInvokePoint.before(BootstrapInvokePoint.Type.BREWING_RECIPES);
+	}
+
+	@Override
+	public void registerBootstrap() {
 		InvisibilityPotions.EFFICIENT_INVISIBILITY.addBrewingRecipe(Potions.INVISIBILITY, Items.GLOWSTONE_DUST);
 		InvisibilityPotions.VERY_EFFICIENT_INVISIBILITY.addBrewingRecipe(InvisibilityPotions.EFFICIENT_INVISIBILITY, Items.QUARTZ);
 		InvisibilityPotions.STRONG_INVISIBILITY.addBrewingRecipe(InvisibilityPotions.VERY_EFFICIENT_INVISIBILITY, Items.GLOWSTONE);
